232f98d62e811df835f0f436470a97fb99217c6a
Mac <louis.niuniu@gmail.com>
2022-11-09 13:39:34 +0100

    updata mybatis

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,19 @@ package org.example;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.example.mapper.InformationMapper;
+import org.example.pojo.Information;
 
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.module.Configuration;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -19,6 +23,13 @@ public class Main {
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory =
                 new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        InformationMapper mapper = sqlSession.getMapper(InformationMapper.class);
+
+        List<Information> information = mapper.selectBlog();
+
+        System.out.println(information);
 
 
     }
--- src/main/java/org/example/mapper/InformationMapper.java
+++ src/main/java/org/example/mapper/InformationMapper.java
@@ -1,6 +1,12 @@
 package org.example.mapper;
 
+import org.example.pojo.Information;
+
+import java.util.List;
+
 public interface InformationMapper {
+
+    List<Information> selectBlog();
 
 
 }
