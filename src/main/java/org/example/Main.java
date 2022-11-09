68db88eabbf9481b5432c8ed8cdc4af6520c2d2d
Mac <louis.niuniu@gmail.com>
2022-11-09 13:26:01 +0100

    set up mybatis structure

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,25 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
-        System.out.println("Hello world!");
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.module.Configuration;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+
+
     }
 }
---
+++ src/main/java/org/example/pojo/Information.java
@@ -0,0 +1,87 @@
+package org.example.pojo;
+
+public class Information {
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String address;
+
+
+    public Information() {
+    }
+
+    public Information(String firstName, String lastName, Integer age, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = address;
+    }
+
+    /**
+     * 获取
+     * @return firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * 设置
+     * @param firstName
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * 获取
+     * @return lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * 设置
+     * @param lastName
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * 设置
+     * @param address
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String toString() {
+        return "Information{firstName = " + firstName + ", lastName = " + lastName + ", age = " + age + ", address = " + address + "}";
+    }
+}
